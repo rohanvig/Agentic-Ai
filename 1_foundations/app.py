93e0413371325dc21101078c2073271d6144f4d6
rohanvig <rohanvig777@gmail.com>
2026-05-09 23:50:44 +0530

    Updated code to use Gemini API instead of OpenAI API, updated model names, and updated code to reflect changes in API us

--- 1_foundations/app.py
+++ 1_foundations/app.py
@@ -8,6 +8,9 @@ import gradio as gr
 
 
 load_dotenv(override=True)
+GEMINI_BASE_URL = "https://generativelanguage.googleapis.com/v1beta/openai/"
+google_api_key =os.getenv("GEMINI_API_KEY")
+gemini = OpenAI(base_url=GEMINI_BASE_URL, api_key=google_api_key)
 
 def push(text):
     requests.post(
@@ -76,8 +79,9 @@ tools = [{"type": "function", "function": record_user_details_json},
 class Me:
 
     def __init__(self):
-        self.openai = OpenAI()
-        self.name = "Ed Donner"
+
+        self.gemini = OpenAI(base_url=GEMINI_BASE_URL, api_key=google_api_key)
+        self.name = "Rohan Vig"
         reader = PdfReader("me/linkedin.pdf")
         self.linkedin = ""
         for page in reader.pages:
@@ -116,7 +120,7 @@ If the user is engaging in discussion, try to steer them towards getting in touc
         messages = [{"role": "system", "content": self.system_prompt()}] + history + [{"role": "user", "content": message}]
         done = False
         while not done:
-            response = self.openai.chat.completions.create(model="gpt-4o-mini", messages=messages, tools=tools)
+            response = gemini.chat.completions.create(model="gemini-2.5-flash-lite", messages=messages, tools=tools)
             if response.choices[0].finish_reason=="tool_calls":
                 message = response.choices[0].message
                 tool_calls = message.tool_calls
